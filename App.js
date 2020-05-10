2f875e3813b5ee7d8645243673aeea0ca26d2bc3
amkdr <korotkin.ari@gmail.com>
2020-05-10 16:55:45 -0500

    first commit

--- App.js
+++ App.js
@@ -1,10 +1,13 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, AsyncStorage } from 'react-native';
+import {Buffer} from 'buffer';
+window.localStorage = AsyncStorage;
+global.Buffer = Buffer;
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Does this still work?</Text>
     </View>
   );
 }
